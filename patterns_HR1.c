db00811e9256b1bed1e6ba6a59d571d41a770142
NikhilWaghmode09 <nick13waghmode@gmail.com>
2023-08-04 18:48:56 +0530

    update patterns_HR1.c

--- patterns_HR1.c
+++ patterns_HR1.c
@@ -1,3 +1,19 @@
+/*
+HackerRank problem: https://www.hackerrank.com/challenges/printing-pattern-2/problem
+*/
+
+/*OUTPUT: 5
+5 5 5 5 5 5 5 5 5 
+5 4 4 4 4 4 4 4 5
+5 4 3 3 3 3 3 4 5
+5 4 3 2 2 2 3 4 5 
+5 4 3 2 1 2 3 4 5
+5 4 3 2 2 2 3 4 5
+5 4 3 3 3 3 3 4 5 
+5 4 4 4 4 4 4 4 5
+5 5 5 5 5 5 5 5 5
+*/
+
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
