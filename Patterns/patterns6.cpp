d54261bbe967b29ebaf5979d91e75ab609dbaa8e
NikhilWaghmode09 <nick13waghmode@gmail.com>
2023-08-07 22:24:57 +0530

    update patterns6.cpp, patterns7.cpp and patterns8.cpp

--- Patterns/patterns6.cpp
+++ Patterns/patterns6.cpp
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // Program to generate pattern 6.
 // NOTE: FOR PATTERN PROBLEMS 'i' is used as no. of rows and 'j' is used as no. of columns.
 //  Always look for relation between the row and column with the pattern.
@@ -34,4 +35,42 @@ int main()
     }
     
     return 0;
+=======
+// Program to generate pattern 6.
+// NOTE: FOR PATTERN PROBLEMS 'i' is used as no. of rows and 'j' is used as no. of columns.
+//  Always look for relation between the row and column with the pattern.
+ 
+/* OUTPUT:
+Enter a number: 4
+1 
+2 2
+3 3 3
+4 4 4 4
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    int i = 1;
+
+    while (i <=n)
+    {
+        int j = 1;
+        while(j<=i){  //LOGIC: j<=i.
+        cout << i << " ";
+        j++;
+        }
+    cout << endl;
+    i++;
+    }
+    
+    return 0;
+>>>>>>> 46e931a (update patterns6.cpp, patterns7.cpp and patterns8.cpp)
 }
--- Patterns/patterns7.cpp
+++ Patterns/patterns7.cpp
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // Program to generate pattern 7.
 // NOTE: FOR PATTERN PROBLEMS 'i' is used as no. of rows and 'j' is used as no. of columns.
 //  Always look for relation between the row and column with the pattern.
@@ -37,4 +38,45 @@ int main()
     }
     
     return 0;
+=======
+// Program to generate pattern 7.
+// NOTE: FOR PATTERN PROBLEMS 'i' is used as no. of rows and 'j' is used as no. of columns.
+//  Always look for relation between the row and column with the pattern.
+ 
+/* OUTPUT:
+Enter a number: 4
+1 
+2 3
+3 4 5
+4 5 6 7
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+ 
+    int i = 1;
+
+    while (i <=n)
+    {
+        int count = i;  //Maintain a count variable since we want to keep printing consecutive numbers.
+        
+        int j = 1;
+        while(j<=i){
+        cout << count << " ";
+        count++;
+        j++;
+        }
+    cout << endl;
+    i++;
+    }
+    
+    return 0;
+>>>>>>> 46e931a (update patterns6.cpp, patterns7.cpp and patterns8.cpp)
 }
